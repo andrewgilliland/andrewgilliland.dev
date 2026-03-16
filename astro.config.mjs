462e102934ec39ae3a0900532b470224e0239868
Andrew Gilliland <andrewpgilliland@gmail.com>
2026-03-15 22:49:21 -0500

    feat: adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://andrewgilliland.dev",
   output: "static",
   integrations: [
+    sitemap(),
     react(),
     tailwind({
       applyBaseStyles: false,
